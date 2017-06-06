977accb0bd7b4eb0f497886ce3ca2a3613ae5455
sspinelli <sacha.bba@gmail.com>
2017-06-05 22:56:06 -0300

    changes over the model, add a options

--- products/models.py
+++ products/models.py
@@ -3,14 +3,27 @@ from __future__ import unicode_literals
 
 from django.db import models
 
+MEDITION_CHOICES = (
+    ('gr', 'gramos'),
+    ('kg', 'kilogramos'),
+    ('ltrs', 'litros')
+)
+
 
 class Product(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField(blank=True, null=True)
     quantity = models.IntegerField()
+    medition = models.CharField(
+        max_length=5,
+        choices=MEDITION_CHOICES,
+        null=True,
+        blank=True
+    )
 
     def __str__(self):
-        return self.name + " " + self.quantity
+        return self.name + " " + str(self.quantity) + (
+            self.medition if self.medition else '')
 
     def as_json(self):
         data = {
